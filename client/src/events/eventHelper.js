c4a782b7060b5cdad7c5243a23622afc204f40fc
Rohit Srivastava <Rohitcnf@gmail.com>
2024-02-04 01:19:17 +0530

    Added Success and error section (mock without API)

--- client/src/events/eventHelper.js
+++ client/src/events/eventHelper.js
@@ -2,7 +2,9 @@ import Defines from "../constants/defines";
 import Helper from "../helper/helper";
 
 class EventHelper {
-  constructor() {}
+  constructor() {
+    this.loginSuccessFlag = true;
+  }
   static userLoginPrefenceEventExecute() {
     const MobileContainerStatus = Helper.hasClass(
       Defines.mobileNumberContainerID,
@@ -30,6 +32,28 @@ class EventHelper {
         Defines.UserPrefernceForLoginID,
         Defines.userPrefernceUserId
       );
+    }
+  }
+  static LoginButtonClickEventExecute() {
+    //hide the Login and unhide the pin section
+    Helper.addClass(Defines.loginFormId, Defines.hiddenClassName);
+    Helper.removeClass(Defines.pinSectionId, Defines.hiddenClassName);
+  }
+
+  static pinSubmitButtonClick() {
+    //hide pin section and unhide the success section
+    const loginFlag = true;
+    Helper.addClass(Defines.pinSectionId, Defines.hiddenClassName);
+    if (loginFlag) {
+      Helper.removeClass(Defines.successScreenId, Defines.hiddenClassName);
+    } else {
+      Helper.removeClass(Defines.errorScreenId, Defines.hiddenClassName);
+      setTimeout(() => {
+        //Take the user to main login Page .
+        //and do all the needful ->>> means hide all sections, and unhide login section only
+        Helper.addClass(Defines.errorScreenId, Defines.hiddenClassName);
+        Helper.removeClass(Defines.loginFormId, Defines.hiddenClassName);
+      }, 5000);
     }
   }
 }
